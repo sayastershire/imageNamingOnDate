e7c0f5ca195ecad7be0a5fa1a7b000382358ddc1
Alvin Januar <aljan_r@outlook.co.id>
2019-11-30 08:43:47 +0700

    baguvix

--- nameDate.sh
+++ nameDate.sh
@@ -15,48 +15,24 @@ for file in *; do
     percent=$(echo "scale=4;100/$totalFile*$curFile" | bc -l)
     echo -ne "Progress: $percent% | ${curInput}${file} | Hit $hitCounter times\r"
     curFile=$(($curFile+1))
-    case "${preferredMethod}" in
-    1)
-        curInputRaw=$(identify -verbose "$file" | grep 'Created Date')
-        curInput=${curInputRaw:26:11}
-        mv $file "${curInput}-$file"
-        hitCounter=$(($hitCounter+1))
-        #echo $curInput
-        ;;
-    2)
+    if ! [[ ${curInput} =~ ^[0-9]+ ]]
+    then
         curInputRaw=$(identify -verbose "$file" | grep 'exif:DateTimeOriginal')
-        curInput=${curInputRaw:19:11}
+        curInput=${curInputRaw:27:10}
         curInputAltered=${curInput//:/}
-        #echo $curInputAltered
-        if [ "${curInputAltered}" != "" ]
+        if [ "${curInputAltered}" == "" ]
         then
+            continue
+        else
             mv $file "${curInputAltered}-$file"
             hitCounter=$(($hitCounter+1))
+            #echo $curInputAltered-$file DateTimeOri
+            preferredMethod=2
         fi
-        ;;
-    *)
-        curInputRaw=$(identify -verbose "$file" | grep 'Created Date')
-        curInput=${curInputRaw:26:11}
-        if [ "${curInput}" == "" ]
-        then
-            curInputRaw=$(identify -verbose "$file" | grep 'exif:DateTime')
-            curInput=${curInputRaw:19:11}
-            curInputAltered=${curInput//:/}
-            if [ "${curInputAltered}" == "" ]
-            then
-                continue
-            else
-                mv $file "${curInputAltered}-$file"
-                hitCounter=$(($hitCounter+1))
-                #echo $curInputAltered
-                preferredMethod=2
-            fi
-        else    
-            mv $file "${curInput}-$file"
-            hitCounter=$(($hitCounter+1))
-            #echo $curInput
-            preferredMethod=1
-        fi
-    esac
-    echo -ne "\r"
+    else    
+        mv $file "${curInput}-$file"
+        hitCounter=$(($hitCounter+1))
+        #echo $curInput-$file Fallback
+        preferredMethod=1
+    fi
 done
